fe475f03235e356023b78e8a3ca4b7d6dc709750
eunbeann <ds5bean23@gmail.com>
2024-08-13 16:33:02 +0900

    style: setting again

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
 import Router from './Router';
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Router />
-      </>
-    );
-  }
+
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
 }
 
 export default App;
