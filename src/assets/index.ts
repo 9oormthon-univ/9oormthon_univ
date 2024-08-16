0f5081f63432349f4ea94212ae39d49bc60e6974
eunbeann <ds5bean23@gmail.com>
2024-08-16 16:15:45 +0900

    style: add svgs

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite-plugin-svgr/client" />
+import GoormBlackBI from './svgs/goorm_black_BI.svg?react';
+import GoormWhiteBI from './svgs/goorm_white_BI.svg?react';
+
+export { GoormBlackBI, GoormWhiteBI };
