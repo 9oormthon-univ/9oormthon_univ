300381899e68edb049768812dad28396bacda470
gracekim527 <lemonherb0323@naver.com>
2024-08-20 00:05:30 +0900

    :lipstick: Design: 리크루트 유의사항 부분 UI 구현 (#2)

--- src/components/recruit/recruitCaution/RecruitCaution.tsx
+++ src/components/recruit/recruitCaution/RecruitCaution.tsx
@@ -1,94 +1,70 @@
-import { motion } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import RecuritCautionRowBox from '../recuritCautionRowBox/RecuritCautionRowBox';
+import classNames from 'classnames/bind';
+import styles from './RecruitCaution.module.scss';
+import calendarGraphic from '../../../assets/svgs/img-calendar.svg';
+import eventGraphic from '../../../assets/svgs/img-event.svg';
+import studyGraphic from '../../../assets/svgs/img-study.svg';
+import { Button } from '@goorm-dev/vapor-components';
+import { ChevronRightIcon } from '@goorm-dev/vapor-icons';
 
-const CautionWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-`;
-
-const BodyTitle = styled.h2`
-  margin-bottom: 2.25rem;
-`;
+const cx = classNames.bind(styles);
 
 function RecruitCaution() {
-  const [visibleBoxes, setVisibleBoxes] = useState({
-    box0: false,
-    box1: false,
-    box2: false,
-    box3: false,
-    box4: false,
-    box5: false,
-  });
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY || document.documentElement.scrollTop;
-      setVisibleBoxes({
-        box0: scrollY >= 900,
-        box1: scrollY >= 1100,
-        box2: scrollY >= 1150,
-        box3: scrollY >= 1200,
-        box4: scrollY >= 1250,
-        box5: scrollY >= 1300,
-        box6: scrollY >= 1350,
-      });
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
-    <>
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={visibleBoxes.box0 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-        transition={{ duration: 0.85 }}
-      >
-        <BodyTitle>유의사항</BodyTitle>
-      </motion.div>
-
-      <CautionWrapper>
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={visibleBoxes.box1 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-          transition={{ duration: 0.85 }}
-        >
-          <RecuritCautionRowBox title="활동 기간" detail={`6월부터 12월까지 \n 활동 가능해야 합니다.`} />
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={visibleBoxes.box2 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-          transition={{ duration: 0.85 }}
-        >
-          <RecuritCautionRowBox title="스터디" detail="유니브별로 진행되는 스터디에 성실하게 참여해야 합니다." />
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={visibleBoxes.box3 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-          transition={{ duration: 0.85 }}
-        >
-          <RecuritCautionRowBox
-            title="KDC/KDT"
-            detail="3,4학년은 KDC/KDT 수강 가능생으로 1,2학년은 자유 모집가능합니다."
-          />
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={visibleBoxes.box4 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-          transition={{ duration: 0.85 }}
-        >
-          <RecuritCautionRowBox title="행사 참여" detail="OT와 단풍톤에 필수적으로 참여해야 합니다." />
-        </motion.div>
-      </CautionWrapper>
-    </>
+    <div className={cx('container')}>
+      <h2 className={cx('titleText')}>유의 사항</h2>
+      <div>
+        <div className={cx('rowLayout')}>
+          {/* 카드 UI */}
+          <div className={cx('cardLayout')}>
+            <div>
+              <h5 className={cx('cardTitleText')}>활동 기간</h5>
+              <p className={cx('cardParagraph')}>
+                6월부터 12월까지 <br />
+                활동 가능해야 합니다.
+              </p>
+            </div>
+            <div>
+              <img className={cx('img')} src={calendarGraphic} alt="달력그래픽" />
+            </div>
+          </div>
+          {/* 카드 UI */}
+          <div className={cx('cardLayout')}>
+            <div>
+              <h5 className={cx('cardTitleText')}>행사 참여</h5>
+              <p className={cx('cardParagraph')}>
+                OT와 단풍톤에 <br />
+                필수적으로 참여해야 합니다.
+              </p>
+            </div>
+            <div>
+              <img className={cx('img')} src={eventGraphic} alt="해커톤그래픽" />
+            </div>
+          </div>
+        </div>
+        {/* 카드 UI */}
+        <div className={cx('cardLayout', 'widthFull')}>
+          <div>
+            <h5 className={cx('cardTitleText')}>스터디 참여</h5>
+            <p className={cx('cardParagraph')}>
+              KDC/KDT 1개 이상 수강 및 스터디 (3~4학년) <br />
+              구름EDU 강의 1개 이상 수강 및 스터디(1~2학년)
+            </p>
+            <Button
+              color="primary"
+              size="lg"
+              tag="button"
+              iconSide="right"
+              icon={ChevronRightIcon}
+              className={cx('button')}>
+              KDC/KDT 자세히 보기
+            </Button>
+          </div>
+          <div>
+            <img src={studyGraphic} alt="스터디그래픽" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
