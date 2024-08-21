cae2b9a1318db7c2213acc27de8df9401d1c6807
eunbeann <ds5bean23@gmail.com>
2024-08-21 11:06:52 +0900

    feat: about mainBanner

--- src/components/about/mainBanner/MainBanner.tsx
+++ src/components/about/mainBanner/MainBanner.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import MainBannerSlogan from '../../../assets/svgs/main_banner_slogan.svg';
+import classNames from 'classnames/bind';
 import MainBannerImg from '../../../assets/svgs/MainBannerImg';
 import MainBannerSmallImg from '../../../assets/svgs/MainBannerSmallImg';
-import classNames from 'classnames/bind';
+import MainBannerSlogan from '../../../assets/svgs/main_banner_slogan.svg';
 import styles from './MainBanner.module.scss';
 
 const cx = classNames.bind(styles);
 
-export default function MainBanner({ scrollTrigger }) {
+interface MainBannerProps {
+  scrollTrigger: () => void;
+}
+
+export default function MainBanner({ scrollTrigger }: MainBannerProps) {
   return (
     <div
-      className={cx('mainBanner', 'd-flex flex-column justify-content-between align-items-center position-fixed w-100')}
-    >
+      className={cx(
+        'mainBanner',
+        'd-flex flex-column justify-content-between align-items-center position-fixed w-100',
+      )}>
       <div className={cx('headerText', 'd-flex flex-column')}>
-        <h3 className="d-none d-md-block text-center">사계절, 구름과 함께</h3>
-        <h5 className="d-block d-md-none text-center">사계절, 구름과 함께</h5>
+        <h3 className="d-none d-md-block text-center">사계절, 구름톤 유니브와 함께</h3>
+        <h5 className="d-block d-md-none text-center">사계절, 구름톤 유니브와 함께</h5>
         <img className={cx('title')} src={MainBannerSlogan} alt="being all seasons with goorm" />
       </div>
       <div>
