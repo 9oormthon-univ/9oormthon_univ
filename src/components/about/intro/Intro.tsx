cd5e15ea1a257408d8287b8c52c1061f368062df
eunbeann <ds5bean23@gmail.com>
2024-08-21 11:06:39 +0900

    style: about intro

--- src/components/about/intro/Intro.tsx
+++ src/components/about/intro/Intro.tsx
@@ -4,35 +4,32 @@ import classNames from 'classnames/bind';
 import styles from './Intro.module.scss';
 
 import BeotkkotImg from '../../../assets/images/about/img-beotkkot-default.png';
-import GoormImg from '../../../assets/images/about/img-goorm-default.png';
 import DanpoongImg from '../../../assets/images/about/img-danpoong-default.png';
+import GoormImg from '../../../assets/images/about/img-goorm-default.png';
 
 import BeotkkotHoverImg from '../../../assets/images/about/img-beotkkot-hover.png';
-import GoormHoverImg from '../../../assets/images/about/img-goorm-hover.png';
 import DanpoongHoverImg from '../../../assets/images/about/img-danpoong-hover.png';
+import GoormHoverImg from '../../../assets/images/about/img-goorm-hover.png';
 
 const cx = classNames.bind(styles);
 
-export default function Intro({ scrollTarget }) {
+interface IntroProps {
+  scrollTarget: React.RefObject<HTMLDivElement>;
+}
+
+const TEXT = `카카오와 구름, 그리고 벚꽃과 단풍이 함께하는 \n 전국 대학 IT 연합 동아리입니다.`;
+
+export default function Intro({ scrollTarget }: IntroProps) {
   return (
     <div
       ref={scrollTarget}
-      className={cx('intro', 'h-100 position-relative d-flex flex-column justify-contents-center align-items-center')}
-    >
+      className={cx('intro', 'h-100 position-relative d-flex flex-column justify-contents-center align-items-center')}>
       <div className={cx('cloudUp', 'w-100 position-absolute')} />
       <h5 className={cx('title', 'd-none d-md-block text-align-center mt-4')}>9oormthonUNIV 는</h5>
       <h6 className={cx('title', 'd-block d-md-none text-align-center mt-4')}>9oormthonUNIV 는</h6>
       <div className="d-flex flex-column">
-        <h3 className={cx('description', 'd-none d-md-block')}>
-          봄과 가을을 기수로 하여
-          <br />
-          아이디어 실현의 장을 제공 해주는 IT 연합 동아리 입니다.
-        </h3>
-        <h4 className={cx('description', 'd-block d-md-none')}>
-          봄과 가을을 기수로 하여
-          <br />
-          아이디어 실현의 장을 제공 해주는 IT 연합 동아리 입니다.
-        </h4>
+        <h3 className={cx('description', 'd-none d-md-block')}>{TEXT}</h3>
+        <h4 className={cx('description', 'd-block d-md-none')}> {TEXT}</h4>
       </div>
       <div className="w-100 d-flex justify-content-center">
         <figure className={cx('imgs')}>
