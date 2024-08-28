baf3510e22d4ae26332b2f864f87ceed93a74e7f
eunbeann <ds5bean23@gmail.com>
2024-08-28 14:37:10 +0900

    feat: feat UnivScrolling

--- src/components/recruit/recruitUnivScrolling/RecruitUnivScrolling.tsx
+++ src/components/recruit/recruitUnivScrolling/RecruitUnivScrolling.tsx
@@ -1,107 +1,36 @@
-import { SearchInput } from '@goorm-dev/gds-components';
-import { useState } from 'react';
-import styled, { keyframes } from 'styled-components';
-import Universities from '../../../utilities/UnivData';
-import UniversityItem from '../UniversityItem/UniversityItem';
+import styles from './RecruitUnivScrolling.module.scss';
 
-const slideLeft = keyframes`
-  0% {
-    transform: translateX(0);
-  }
-  100% {
-    transform: translateX(-50%);
-  }
-`;
+interface itemsType {
+  src: string;
+  altText: string;
+}
 
-const slideRight = keyframes`
-  0% {
-    transform: translateX(0);
-  }
-  100% {
-    transform: translateX(50%);
-  }
-`;
+const items: itemsType[] = [
+  {
+    src: '/src/assets/svgs/univList1.svg',
+    altText: 'Slide 1',
+  },
+  {
+    src: '/src/assets/svgs/univList2.svg',
+    altText: 'Slide 2',
+  },
+  {
+    src: '/src/assets/svgs/univList3.svg',
+    altText: 'Slide 3',
+  },
+];
 
-const ImageSlider = styled.div`
-  display: flex;
-  animation: ${slideLeft} 150s linear infinite;
-  overflow: hidden;
-
-  &:hover {
-    animation-play-state: paused;
-  }
-  margin-bottom: 1rem;
-`;
-
-const ImageSlider2 = styled.div`
-  display: flex;
-  animation: ${slideRight} 150s linear infinite;
-  overflow: hidden;
-
-  &:hover {
-    animation-play-state: paused;
-  }
-`;
-
-const SearchInputCustom = styled(SearchInput)`
-  margin-bottom: 6rem;
-  width: 20rem;
-  height: 3.5rem;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-  width: 100%;
-`;
-
-const SearchContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ShadowBgContainer = styled.div`
-  filter: drop-shadow(var(--gray-200) 0px 8px 40px);
-`;
-
-function RecruitUnivScrolling({ searchable }) {
-  const [searchQuery, setSearchQuery] = useState('');
-  const filteredUniversities = Universities.filter((univ) =>
-    univ.name.toLowerCase().includes(searchQuery.toLowerCase()),
-  );
-
-  // 슬라이더 아이템 복제
-  const firstHalfUniversities = filteredUniversities.slice(0, Math.ceil(filteredUniversities.length / 2));
-  const secondHalfUniversities = filteredUniversities.slice(Math.ceil(filteredUniversities.length / 2));
-  const duplicatedFirstHalf = [...firstHalfUniversities, ...firstHalfUniversities, ...firstHalfUniversities];
-  const duplicatedSecondHalf = [...secondHalfUniversities, ...secondHalfUniversities, ...secondHalfUniversities];
-
+function RecruitUnivScrolling() {
   return (
-    <ShadowBgContainer className="d-flex flex-column">
-      <div className="d-flex justify-content-center align-items-center"></div>
-      {searchQuery ? (
-        <SearchContainer>
-          {filteredUniversities.map((univ, index) => (
-            <UniversityItem key={index} image={univ.image} name={univ.name} link={univ.link} />
-          ))}
-        </SearchContainer>
-      ) : (
-        <Container>
-          <ImageSlider>
-            {duplicatedFirstHalf.map((univ, index) => (
-              <UniversityItem key={index} image={univ.image} name={univ.name} link={univ.link} />
-            ))}
-          </ImageSlider>
-          <ImageSlider2>
-            {duplicatedSecondHalf.map((univ, index) => (
-              <UniversityItem key={index} image={univ.image} name={univ.name} link={univ.link} />
-            ))}
-          </ImageSlider2>
-        </Container>
-      )}
-    </ShadowBgContainer>
+    <div className={styles.backgroundWrapper}>
+      <div className={styles.carouselWrapper}>
+        {items.map((item, index) => (
+          <div className={index === 1 ? styles.autoCarouselBoxRight : styles.autoCarouselBox}>
+            <img src={item.src} alt={item.altText} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
