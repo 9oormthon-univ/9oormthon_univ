42f67d33ffafd415207690dac9fc58ae523648ac
gracekim527 <lemonherb0323@naver.com>
2024-08-29 16:46:49 +0900

    :lipstick: Style: Recruit Calendar breakpoint 구현 (#13)

--- src/components/recruit/recruitCalendar/RecuritCalendar.tsx
+++ src/components/recruit/recruitCalendar/RecuritCalendar.tsx
@@ -17,43 +17,53 @@ function RecuritCalendar() {
         </div>
         <div className={cx('leftSection')}>
           <div className={cx('leftBottomSection')}>
-            <div className={cx('scheduleDiv')}>
-              <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
-                1. 대표 모집 :
-              </Text>
-              <Text color="text-hint" typography="subtitle2" fontWeight="medium">
-                1월 21일 ~ 1월 30일
-              </Text>
+            <div className={cx('sm-Schedule')}>
+              <div className={cx('scheduleDiv')}>
+                <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
+                  1. 대표 모집 :
+                </Text>
+                <Text color="text-hint" typography="subtitle2" fontWeight="medium">
+                  1월 21일 ~ 1월 30일
+                </Text>
+              </div>
+              <div className={cx('scheduleDiv')}>
+                <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
+                  2. 대표 OT :
+                </Text>
+                <Text color="text-hint" typography="subtitle2" fontWeight="medium">
+                  2월 17일
+                </Text>
+              </div>
             </div>
-            <div className={cx('scheduleDiv')}>
-              <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
-                2. 대표 OT :
-              </Text>
-              <Text color="text-hint" typography="subtitle2" fontWeight="medium">
-                2월 17일
-              </Text>
-            </div>
-            <div className={cx('scheduleDiv')}>
-              <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
-                3. 미르미 모집 :
-              </Text>
-              <Text color="text-hint" typography="subtitle2" fontWeight="medium">
-                1월 21일 - 2월 11일
-              </Text>
-            </div>
-            <div className={cx('scheduleDiv')}>
-              <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
-                4. 전체 OT
-              </Text>
-              <Text color="text-hint" typography="subtitle2" fontWeight="medium">
-                2월 17일
-              </Text>
+            <div className={cx('sm-Schedule')}>
+              <div className={cx('scheduleDiv')}>
+                <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
+                  3. 미르미 모집 :
+                </Text>
+                <Text color="text-hint" typography="subtitle2" fontWeight="medium">
+                  1월 21일 - 2월 11일
+                </Text>
+              </div>
+              <div className={cx('scheduleDiv')}>
+                <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
+                  4. 전체 OT :
+                </Text>
+                <Text color="text-hint" typography="subtitle2" fontWeight="medium">
+                  2월 17일
+                </Text>
+              </div>
             </div>
           </div>
           <div className={cx('leftTopSection')}>
-            <Text color="text-hint" typography="subtitle1" fontWeight="medium">
+            <Text className={cx('show-on-lg')} color="text-hint" typography="subtitle1" fontWeight="medium">
               유니브 별로 모집일정이 상이하니
               <br /> 자세한 일정은 각 유니브 인스타를 참고하세요
+            </Text>
+            <Text className={cx('show-on-md')} as="span" color="text-hint" typography="subtitle2" fontWeight="medium">
+              유니브 별로 모집일정이 상이하니 자세한 일정은 각 유니브 인스타를 참고하세요
+            </Text>
+            <Text className={cx('show-on-xs')} as="span" color="text-hint" typography="subtitle2" fontWeight="medium">
+              유니브 별로 모집일정이 상이하니 자세한 일정은 <br /> 각 유니브 인스타를 참고하세요
             </Text>
           </div>
         </div>
