dfa38b74e2e2758e7e2eda62d3f5fb4610866ab7
gracekim527 <lemonherb0323@naver.com>
2024-08-29 17:03:16 +0900

    :lipstick: Style: Recruit Target breakpoint 구현 (#13)

--- src/components/recruit/recuritTarget/RecuritTarget.tsx
+++ src/components/recruit/recuritTarget/RecuritTarget.tsx
@@ -16,21 +16,34 @@ function RecuritTarget() {
       <div className={cx('bottomSection')}>
         <div className={cx('targetBox')}>
           <img src={ProgrammingIcon} />
-          <Text as="h4" color="text-alternative" typography="heading4" fontWeight="bold" className={cx('contentsText')}>
+          <Text className={cx('show-on-lg')} as="h4" color="text-alternative" typography="heading4" fontWeight="bold">
+            IT 서비스의 구현에
+            <br /> 관심이 많은 대학생
+          </Text>
+
+          <Text className={cx('show-on-md')} as="h5" color="text-alternative" typography="heading5" fontWeight="bold">
             IT 서비스의 구현에
             <br /> 관심이 많은 대학생
           </Text>
         </div>
         <div className={cx('targetBox')}>
           <img src={ExamIcon} />
-          <Text as="h4" color="text-alternative" typography="heading4" fontWeight="bold" className={cx('contentsText')}>
+          <Text className={cx('show-on-lg')} as="h4" color="text-alternative" typography="heading4" fontWeight="bold">
+            타 학교 친구들과 <br />
+            협업 경험을 쌓고 싶은 대학생
+          </Text>
+          <Text className={cx('show-on-md')} as="h5" color="text-alternative" typography="heading5" fontWeight="bold">
             타 학교 친구들과 <br />
             협업 경험을 쌓고 싶은 대학생
           </Text>
         </div>
         <div className={cx('targetBox')}>
           <img src={SunIcon} />
-          <Text as="h4" color="text-alternative" typography="heading4" fontWeight="bold" className={cx('contentsText')}>
+          <Text className={cx('show-on-lg')} as="h4" color="text-alternative" typography="heading4" fontWeight="bold">
+            열정과 성실함이 <br />
+            넘치는 대학생
+          </Text>
+          <Text className={cx('show-on-md')} as="h5" color="text-alternative" typography="heading5" fontWeight="bold">
             열정과 성실함이 <br />
             넘치는 대학생
           </Text>
