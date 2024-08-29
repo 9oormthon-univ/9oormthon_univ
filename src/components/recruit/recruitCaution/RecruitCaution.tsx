6080b67ad57deeccdd338a94ac3c7a7cf7ebec23
gracekim527 <lemonherb0323@naver.com>
2024-08-29 17:22:06 +0900

    :lipstick: Style: Recruit Caution breakpoint 구현 (#13)

--- src/components/recruit/recruitCaution/RecruitCaution.tsx
+++ src/components/recruit/recruitCaution/RecruitCaution.tsx
@@ -20,10 +20,18 @@ function RecruitCaution() {
           {/* 카드 UI */}
           <div className={cx('cardLayout')}>
             <div className={cx('textLayout')}>
-              <Text as="h5" color="text-normal" typography="heading5" fontWeight="bold" className={cx('cardTitleText')}>
+              {/* 반응형 적용 */}
+              <Text className={cx('show-on-sm')} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
                 활동 기간
               </Text>
-              <Text as="p" color="text-hint" typography="body1" fontWeight="regular" className={cx('cardParagraph')}>
+              <Text className={cx('show-on-xs')} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
+                활동 기간
+              </Text>
+              <Text className={cx('show-on-sm')} as="p" color="text-hint" typography="body1" fontWeight="regular">
+                6월부터 12월까지 <br />
+                활동 가능해야 합니다.
+              </Text>
+              <Text className={cx('show-on-xs')} as="p" color="text-hint" typography="body3" fontWeight="regular">
                 6월부터 12월까지 <br />
                 활동 가능해야 합니다.
               </Text>
@@ -35,10 +43,18 @@ function RecruitCaution() {
           {/* 카드 UI */}
           <div className={cx('cardLayout')}>
             <div className={cx('textLayout')}>
-              <Text as="h5" color="text-normal" typography="heading5" fontWeight="bold" className={cx('cardTitleText')}>
+              <Text className={cx('show-on-sm')} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
                 행사 참여
               </Text>
-              <Text as="p" color="text-hint" typography="body1" fontWeight="regular" className={cx('cardParagraph')}>
+              <Text className={cx('show-on-xs')} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
+                행사 참여
+              </Text>
+
+              <Text className={cx('show-on-sm')} as="p" color="text-hint" typography="body1" fontWeight="regular">
+                OT와 단풍톤에 <br />
+                필수적으로 참여해야 합니다.
+              </Text>
+              <Text className={cx('show-on-xs')} as="p" color="text-hint" typography="body3" fontWeight="regular">
                 OT와 단풍톤에 <br />
                 필수적으로 참여해야 합니다.
               </Text>
@@ -52,10 +68,17 @@ function RecruitCaution() {
         <div className={cx('cardLayout', 'widthFull')}>
           <div className={cx('buttonLayout')}>
             <div className={cx('textLayout')}>
-              <Text as="h5" color="text-normal" typography="heading5" fontWeight="bold" className={cx('cardTitleText')}>
+              <Text className={cx('show-on-sm')} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
                 스터디 참여
               </Text>
-              <Text as="p" color="text-hint" typography="body1" fontWeight="regular" className={cx('cardParagraph')}>
+              <Text className={cx('show-on-xs')} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
+                스터디 참여
+              </Text>
+              <Text className={cx('show-on-sm')} as="p" color="text-hint" typography="body1" fontWeight="regular">
+                KDC/KDT 1개 이상 수강 및 스터디 (3~4학년) <br />
+                구름EDU 강의 1개 이상 수강 및 스터디(1~2학년)
+              </Text>
+              <Text className={cx('show-on-xs')} as="p" color="text-hint" typography="body3" fontWeight="regular">
                 KDC/KDT 1개 이상 수강 및 스터디 (3~4학년) <br />
                 구름EDU 강의 1개 이상 수강 및 스터디(1~2학년)
               </Text>
@@ -73,7 +96,7 @@ function RecruitCaution() {
             </div>
           </div>
 
-          <div>
+          <div className={cx('show-on-lg')}>
             <img src={studyGraphic} alt="스터디그래픽" />
           </div>
         </div>
