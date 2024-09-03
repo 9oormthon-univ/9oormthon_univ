e2ba6ee3fafbb84bd689cc36df96e43685aa2a2b
jaminleee <jamin_00@naver.com>
2024-09-03 17:09:39 +0900

    [Refactor/#29] Refactor cx style

--- src/components/recruit/recruitCalendar/RecuritCalendar.tsx
+++ src/components/recruit/recruitCalendar/RecuritCalendar.tsx
@@ -1,24 +1,21 @@
-import classNames from 'classnames/bind';
 import styles from './RecruitCalendar.module.scss';
 import newCalendar from '../../../assets/images/newCalendar.png';
 import { Text } from '@goorm-dev/vapor-components';
 
-const cx = classNames.bind(styles);
-
 function RecuritCalendar() {
   return (
-    <div className={cx('container')}>
+    <div className={styles.container}>
       <Text as="h3" color="gray-900" typography="heading3" fontWeight="bold">
         3기 모집 일정
       </Text>
-      <div className={cx('wrapper')}>
-        <div className={cx('rightSection')}>
-          <img className={cx('calendarImg')} src={newCalendar} alt="4기 달력" />
+      <div className={styles.wrapper}>
+        <div className={styles.rightSection}>
+          <img className={styles.calendarImg} src={newCalendar} alt="4기 달력" />
         </div>
-        <div className={cx('leftSection')}>
-          <div className={cx('leftBottomSection')}>
-            <div className={cx('sm-Schedule')}>
-              <div className={cx('scheduleDiv')}>
+        <div className={styles.leftSection}>
+          <div className={styles.leftBottomSection}>
+            <div className={styles.smSchedule}>
+              <div className={styles.scheduleDiv}>
                 <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
                   1. 대표 모집 :
                 </Text>
@@ -26,7 +23,7 @@ function RecuritCalendar() {
                   1월 21일 ~ 1월 30일
                 </Text>
               </div>
-              <div className={cx('scheduleDiv')}>
+              <div className={styles.scheduleDiv}>
                 <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
                   2. 대표 OT :
                 </Text>
@@ -35,8 +32,8 @@ function RecuritCalendar() {
                 </Text>
               </div>
             </div>
-            <div className={cx('sm-Schedule')}>
-              <div className={cx('scheduleDiv')}>
+            <div className={styles.smSchedule}>
+              <div className={styles.scheduleDiv}>
                 <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
                   3. 미르미 모집 :
                 </Text>
@@ -44,7 +41,7 @@ function RecuritCalendar() {
                   1월 21일 - 2월 11일
                 </Text>
               </div>
-              <div className={cx('scheduleDiv')}>
+              <div className={styles.scheduleDiv}>
                 <Text color="text-alternative" typography="subtitle1" fontWeight="medium">
                   4. 전체 OT :
                 </Text>
@@ -54,15 +51,15 @@ function RecuritCalendar() {
               </div>
             </div>
           </div>
-          <div className={cx('leftTopSection')}>
-            <Text className={cx('show-on-lg')} color="text-hint" typography="subtitle1" fontWeight="medium">
+          <div className={styles.leftTopSection}>
+            <Text className={styles.showOnLg} color="text-hint" typography="subtitle1" fontWeight="medium">
               유니브 별로 모집일정이 상이하니
               <br /> 자세한 일정은 각 유니브 인스타를 참고하세요
             </Text>
-            <Text className={cx('show-on-md')} as="span" color="text-hint" typography="subtitle2" fontWeight="medium">
+            <Text className={styles.showOnMd} as="span" color="text-hint" typography="subtitle2" fontWeight="medium">
               유니브 별로 모집일정이 상이하니 자세한 일정은 각 유니브 인스타를 참고하세요
             </Text>
-            <Text className={cx('show-on-xs')} as="span" color="text-hint" typography="subtitle2" fontWeight="medium">
+            <Text className={styles.showOnXs} as="span" color="text-hint" typography="subtitle2" fontWeight="medium">
               유니브 별로 모집일정이 상이하니 자세한 일정은 <br /> 각 유니브 인스타를 참고하세요
             </Text>
           </div>
--- src/components/recruit/recruitCaution/RecruitCaution.tsx
+++ src/components/recruit/recruitCaution/RecruitCaution.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames/bind';
 import styles from './RecruitCaution.module.scss';
 import calendarGraphic from '../../../assets/svgs/img-calendar.svg';
 import eventGraphic from '../../../assets/svgs/img-event.svg';
@@ -7,78 +6,76 @@ import { Button } from '@goorm-dev/vapor-components';
 import { ChevronRightIcon } from '@goorm-dev/vapor-icons';
 import { Text } from '@goorm-dev/vapor-components';
 
-const cx = classNames.bind(styles);
-
 function RecruitCaution() {
   return (
-    <div className={cx('container')}>
+    <div className={styles.container}>
       <Text as="h3" color="text-normal" typography="heading3" fontWeight="bold">
         유의 사항
       </Text>
       <div>
-        <div className={cx('rowLayout')}>
+        <div className={styles.rowLayout}>
           {/* 카드 UI */}
-          <div className={cx('cardLayout')}>
-            <div className={cx('textLayout')}>
+          <div className={styles.cardLayout}>
+            <div className={styles.textLayout}>
               {/* 반응형 적용 */}
-              <Text className={cx('show-on-sm')} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
+              <Text className={styles.showOnSm} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
                 활동 기간
               </Text>
-              <Text className={cx('show-on-xs')} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
+              <Text className={styles.showOnXs} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
                 활동 기간
               </Text>
-              <Text className={cx('show-on-sm')} as="p" color="text-hint" typography="body1" fontWeight="regular">
+              <Text className={styles.showOnSm} as="p" color="text-hint" typography="body1" fontWeight="regular">
                 6월부터 12월까지 <br />
                 활동 가능해야 합니다.
               </Text>
-              <Text className={cx('show-on-xs')} as="p" color="text-hint" typography="body3" fontWeight="regular">
+              <Text className={styles.showOnXs} as="p" color="text-hint" typography="body3" fontWeight="regular">
                 6월부터 12월까지 <br />
                 활동 가능해야 합니다.
               </Text>
             </div>
             <div>
-              <img className={cx('img')} src={calendarGraphic} alt="달력그래픽" />
+              <img className={styles.img} src={calendarGraphic} alt="달력그래픽" />
             </div>
           </div>
           {/* 카드 UI */}
-          <div className={cx('cardLayout')}>
-            <div className={cx('textLayout')}>
-              <Text className={cx('show-on-sm')} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
+          <div className={styles.cardLayout}>
+            <div className={styles.textLayout}>
+              <Text className={styles.showOnSm} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
                 행사 참여
               </Text>
-              <Text className={cx('show-on-xs')} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
+              <Text className={styles.showOnXs} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
                 행사 참여
               </Text>
 
-              <Text className={cx('show-on-sm')} as="p" color="text-hint" typography="body1" fontWeight="regular">
+              <Text className={styles.showOnSm} as="p" color="text-hint" typography="body1" fontWeight="regular">
                 OT와 단풍톤에 <br />
                 필수적으로 참여해야 합니다.
               </Text>
-              <Text className={cx('show-on-xs')} as="p" color="text-hint" typography="body3" fontWeight="regular">
+              <Text className={styles.showOnXs} as="p" color="text-hint" typography="body3" fontWeight="regular">
                 OT와 단풍톤에 <br />
                 필수적으로 참여해야 합니다.
               </Text>
             </div>
             <div>
-              <img className={cx('img')} src={eventGraphic} alt="해커톤그래픽" />
+              <img className={styles.img} src={eventGraphic} alt="해커톤그래픽" />
             </div>
           </div>
         </div>
         {/* 카드 UI */}
-        <div className={cx('cardLayout', 'widthFull')}>
-          <div className={cx('buttonLayout')}>
-            <div className={cx('textLayout')}>
-              <Text className={cx('show-on-sm')} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
+        <div className={`${styles.cardLayout} ${styles.widthFull}`}>
+          <div className={styles.buttonLayout}>
+            <div className={styles.textLayout}>
+              <Text className={styles.showOnSm} as="h5" color="text-normal" typography="heading5" fontWeight="bold">
                 스터디 참여
               </Text>
-              <Text className={cx('show-on-xs')} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
+              <Text className={styles.showOnXs} as="h6" color="text-normal" typography="heading6" fontWeight="medium">
                 스터디 참여
               </Text>
-              <Text className={cx('show-on-sm')} as="p" color="text-hint" typography="body1" fontWeight="regular">
+              <Text className={styles.showOnSm} as="p" color="text-hint" typography="body1" fontWeight="regular">
                 KDC/KDT 1개 이상 수강 및 스터디 (3~4학년) <br />
                 구름EDU 강의 1개 이상 수강 및 스터디(1~2학년)
               </Text>
-              <Text className={cx('show-on-xs')} as="p" color="text-hint" typography="body3" fontWeight="regular">
+              <Text className={styles.showOnXs} as="p" color="text-hint" typography="body3" fontWeight="regular">
                 KDC/KDT 1개 이상 수강 및 스터디 (3~4학년) <br />
                 구름EDU 강의 1개 이상 수강 및 스터디(1~2학년)
               </Text>
@@ -90,13 +87,13 @@ function RecruitCaution() {
                 tag="button"
                 iconSide="right"
                 icon={ChevronRightIcon}
-                className={cx('button')}>
+                className={styles.button}>
                 KDC/KDT 자세히 보기
               </Button>
             </div>
           </div>
 
-          <div className={cx('show-on-lg')}>
+          <div className={styles.showOnLg}>
             <img src={studyGraphic} alt="스터디그래픽" />
           </div>
         </div>
