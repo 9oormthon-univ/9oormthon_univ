72d5334c8b19a9b9e40ffe5c4739dd9c8b6cefde
eunbeann <ds5bean23@gmail.com>
2024-09-03 18:30:19 +0900

    feat: findingUvics

--- src/components/about/findingUniv/FindingUniv.tsx
+++ src/components/about/findingUniv/FindingUniv.tsx
@@ -1,35 +1,8 @@
-import classNames from 'classnames';
-import styled from 'styled-components';
 import { THIS_SEASON } from '../../../constants/common';
 import RecruitUnivScrolling from '../../recruit/recruitUnivScrolling/RecruitUnivScrolling';
 
 import { Text } from '@goorm-dev/vapor-components';
 import styles from './FindingUniv.module.scss';
-
-const cx = classNames.bind(styles);
-
-const Container = styled.div`
-  display: flex;
-  overflow-x: hidden;
-  width: 100%;
-`;
-
-const ImageSlider = styled.div`
-  display: flex;
-  animation: slide 20s linear infinite;
-
-  &:hover {
-    animation-play-state: paused;
-  }
-  @keyframes slide {
-    0% {
-      transform: translateX(0);
-    }
-    100% {
-      transform: translateX(-50%);
-    } // 목록의 절반만큼만 이동
-  }
-`;
 
 const TEXT = `전국 ${THIS_SEASON.AMOUNT_OF_UNIV}개 대학이 \n 구름톤 유니브와 함께하고 있어요`;
 
@@ -40,7 +13,7 @@ function FindingUniv() {
         <Text typography="heading2" className={styles.titleText}>
           {TEXT}
         </Text>
-        <Text typography="heading2" className={styles.titleSmallText}>
+        <Text typography="heading3" className={styles.titleSmallText}>
           {TEXT}
         </Text>
       </div>
