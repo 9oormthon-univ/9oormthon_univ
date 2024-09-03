14120c2a34bba105112c341ed9a6fb3ffdfd924a
eunbeann <ds5bean23@gmail.com>
2024-09-03 18:30:09 +0900

    feat: benefits

--- src/components/about/benefit/BenefitDesktop/BenefitItem.tsx
+++ src/components/about/benefit/BenefitDesktop/BenefitItem.tsx
@@ -2,10 +2,7 @@ import { Link } from 'react-router-dom';
 
 import { BackPageIcon } from '@goorm-dev/gds-icons';
 import { Text } from '@goorm-dev/vapor-components';
-import classNames from 'classnames/bind';
 import styles from './BenefitDesktop.module.scss';
-
-const cx = classNames.bind(styles);
 
 interface BenefitItemProps {
   title: string;
@@ -15,12 +12,12 @@ interface BenefitItemProps {
 
 export default function BenefitItem({ title, description, url }: BenefitItemProps) {
   return (
-    <Link className={cx('benefitLink')} to={url} target="\_blank">
-      <div className={cx('textWrapper')}>
+    <Link className={styles.benefitLink} to={url} target="\_blank">
+      <div className={styles.itemTextWrapper}>
         <Text typography="body2">{description}</Text>
         <Text typography="heading5">{title}</Text>
       </div>
-      <div className={cx('iconWrapper')}>
+      <div className={styles.iconWrapper}>
         <BackPageIcon width="32" height="32" fill="#858899" />
       </div>
     </Link>
