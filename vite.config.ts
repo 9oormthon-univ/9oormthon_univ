33ad53f4c3d0c11a209f5d5c7f19b0360d859570
eunbeann <ds5bean23@gmail.com>
2024-09-05 10:53:43 +0900

    feat: add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // optional, change if needed
+  },
   plugins: [react(), svgr()],
 });
