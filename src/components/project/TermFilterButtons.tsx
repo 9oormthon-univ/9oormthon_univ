9798c5f1d407bfc766d9956448987caaea1d33a2
eunbeann <ds5bean23@gmail.com>
2024-09-05 15:29:35 +0900

    feat: reponsive prok

--- src/components/project/TermFilterButtons.tsx
+++ src/components/project/TermFilterButtons.tsx
@@ -1,17 +1,26 @@
 import { Text } from '@goorm-dev/vapor-components';
 import classNames from 'classnames';
+import { motion } from 'framer-motion';
+import { ComponentProps, ReactNode } from 'react';
 import styles from './projectStyles.module.scss';
 
-interface TermFilterButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+type Props = ComponentProps<typeof motion.button>;
+
+interface TermFilterButtonProps extends Props {
   active: boolean;
+  children: ReactNode;
 }
 
 export default function TermFilterButton({ children, active, ...restProps }: TermFilterButtonProps) {
   return (
-    <button className={classNames(styles.termFilterButton, { [styles.active]: active })} {...restProps}>
+    <motion.button
+      className={classNames(styles.termFilterButton, { [styles.active]: active })}
+      whileTap={{ scale: 0.96 }}
+      transition={{ duration: 0.1 }}
+      {...restProps}>
       <Text typography="subtitle1" color={active ? 'text-primary-alternative' : 'text-alternative'}>
         {children}
       </Text>
-    </button>
+    </motion.button>
   );
 }
