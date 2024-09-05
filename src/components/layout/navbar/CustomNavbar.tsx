f3ceb1ce4d3bd275478f8b599436f1032ff2d765
eunbeann <ds5bean23@gmail.com>
2024-09-05 18:14:22 +0900

    feat: modify QA

--- src/components/layout/navbar/CustomNavbar.tsx
+++ src/components/layout/navbar/CustomNavbar.tsx
@@ -1,5 +1,5 @@
 import { Button, GoormNavbar, Nav, NavItem, NavLink } from '@goorm-dev/gds-components';
-import { OutIcon, SearchIcon } from '@goorm-dev/gds-icons';
+import { ChevronRightIcon, OutIcon } from '@goorm-dev/gds-icons';
 import { useState } from 'react';
 import { GoormBlackBI, GoormWhiteBI } from '../../../assets';
 import { useIsAbout } from '../../../hooks/useIsAbout';
@@ -24,7 +24,7 @@ function CustomNavbar() {
     {
       title: (
         <>
-          univ-log <OutIcon className="mx-1" />
+          UNIV-LOG <OutIcon className="mx-1" />
         </>
       ),
       to: 'https://9oormthonuniv.tistory.com/',
@@ -32,18 +32,18 @@ function CustomNavbar() {
   ];
 
   return (
-    <GoormNavbar isOpened={isOpened} setIsOpened={setIsOpened}>
-      <GoormNavbar.Brand href="/" className="navbar-brand">
-        {isAbout ? <GoormWhiteBI /> : <GoormBlackBI />}
+    <GoormNavbar className={isAbout ? styles.navBarStyle : ''} isOpened={isOpened} setIsOpened={setIsOpened}>
+      <GoormNavbar.Brand href="/" className="ml-2">
+        {isAbout && !isOpened ? <GoormWhiteBI /> : <GoormBlackBI />}
       </GoormNavbar.Brand>
-      <GoormNavbar.Collapse isOpened={isOpened} className="justify-content-start">
-        <Nav pills>
+      <GoormNavbar.Collapse isOpened={isOpened} className={styles.collapse}>
+        <Nav navbar pills className={styles.navbar}>
           {NAV_ITEMS.map((_, index) => (
             <NavItem key={index}>
               <NavLink
                 className={isAbout ? styles.whiteFont : ''}
                 href={_.to}
-                target={_.to.startsWith('http') ? '_blank' : '_self'} // 외부 링크는 새 탭에서 열림
+                target={_.to.startsWith('http') ? '_blank' : '_self'}
                 rel={_.to.startsWith('http') ? 'noopener noreferrer' : undefined}
                 onClick={() => isOpened && setIsOpened((prev) => !prev)}>
                 {_.title}
@@ -51,29 +51,31 @@ function CustomNavbar() {
             </NavItem>
           ))}
         </Nav>
-      </GoormNavbar.Collapse>
-      <GoormNavbar.Collapse isOpened={isOpened} className="justify-content-end">
-        <Nav className="gap-0">
+        <Nav navbar className={styles.navbar}>
           <NavLink
             className={isAbout ? styles.whiteFont : ''}
             href="/search-univ"
             onClick={() => isOpened && setIsOpened((prev) => !prev)}>
-            <SearchIcon className="mx-1" />
             나의 유니브 찾기
           </NavLink>
 
           {isLoggedIn ? (
             <NavLink className={styles.grayCircle} href="/my-page"></NavLink>
           ) : (
-            <Button className={styles.loginButton} size="lg" href="/login">
-              로그인
-            </Button>
+            <>
+              <Button className={styles.loginButton} size="lg" href="/login">
+                로그인
+              </Button>
+              <NavLink className={styles.loginText} href="/login">
+                로그인하기
+                <ChevronRightIcon className="ml-1" />
+              </NavLink>
+            </>
           )}
         </Nav>
       </GoormNavbar.Collapse>
-
       <GoormNavbar.Toggler
-        className="navbar-toggler"
+        className={styles.customToggler}
         toggle={setIsOpened}
         isOpened={isOpened}
         $isAbout={isAbout}
