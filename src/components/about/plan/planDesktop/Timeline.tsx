97372cb6b8550da991ed20f1233605c26f3ab3d3
gracekim527 <lemonherb0323@naver.com>
2025-02-04 11:21:28 +0900

    :bug: Fix: 버튼토글 애니메이션 활성화 (#48)

--- src/components/about/plan/planDesktop/Timeline.tsx
+++ src/components/about/plan/planDesktop/Timeline.tsx
@@ -1,12 +1,9 @@
 import { TIMELINE_DATA } from '../../../../utilities/AboutData.ts';
 
-import classNames from 'classnames/bind';
-
-import { Text } from '@goorm-dev/vapor-components';
+import { ButtonToggleGroup } from '@goorm-dev/vapor-components';
 import CardList from './CardList.tsx';
 import styles from './PlanDesktop.module.scss';
-
-const cx = classNames.bind(styles);
+import { useEffect, useState } from 'react';
 
 export const getMonthText = (key: number) => {
   switch (key) {
@@ -32,20 +29,36 @@ interface TimelineProps {
   handleMonthClick: (month: number) => void;
 }
 
+// 사이즈 정의
+type Size = 'md' | 'xl';
+
+const getSizeByWindowWidth = (width: number) => {
+  if (width <= 540) return 'md';
+  return 'xl';
+};
+
 export default function Timeline({ month, handleMonthClick }: TimelineProps) {
+  const [size, setSize] = useState<Size>(getSizeByWindowWidth(window.innerWidth));
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize(getSizeByWindowWidth(window.innerWidth));
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className={styles.timeline}>
-      <figure className={styles.timelineBar}>
+      <ButtonToggleGroup className={styles.toggleGroup} activeIndex={month} size={size} onToggle={handleMonthClick}>
         {TIMELINE_DATA.map((_, index) => (
-          <div
-            onClick={() => handleMonthClick(index)}
-            className={cx(`monthTextClickable`, `${index === month && 'active'}`)}>
-            <Text typography="heading6" key={index}>
-              {getMonthText(index)}
-            </Text>
-          </div>
+          <ButtonToggleGroup.ButtonToggleItem onClick={() => handleMonthClick(index)}>
+            {getMonthText(index)}
+          </ButtonToggleGroup.ButtonToggleItem>
         ))}
-      </figure>
+      </ButtonToggleGroup>
+
       <CardList month={month} handleMonthClick={handleMonthClick} />
     </div>
   );
