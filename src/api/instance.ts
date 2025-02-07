7eb215be7276d5321a70663bf3c00d9d2a2e04e7
gracekim527 <lemonherb0323@naver.com>
2025-02-07 13:42:03 +0900

    :sparkles: Feat: 인스턴스 일부 추가 (#52)

---
+++ src/api/instance.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
+
+export default instance;
