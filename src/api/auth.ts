f4f540f8d536889fad0f46f33e8a1bc1897bb6ef
gracekim527 <lemonherb0323@naver.com>
2025-02-11 19:09:21 +0900

    :sparkles: Feat: 로그인 관련 api 구현 (#54)

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -8,3 +8,27 @@ export const loginAPI = async (serial_id: string, password: string) => {
 
   return response.data;
 };
+
+// 로그아웃 api
+export const logoutAPI = async () => {
+  const response = await instance.post('/api/v1/auth/logout');
+
+  return response.data;
+};
+
+// JWT 재발급
+export const refreshAPI = async () => {
+  const response = await instance.post('/api/v1/auth/reissue');
+
+  return response.data;
+};
+
+// 비밀번호 재설정
+export const resetPasswordAPI = async (current_password: string, new_password: string) => {
+  const response = await instance.patch('/api/v1/auth/password', {
+    current_password,
+    new_password,
+  });
+
+  return response.data;
+};
