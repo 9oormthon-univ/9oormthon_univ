67ecd4ed5bc0e54e795012906bf54e99f2e52c01
gracekim527 <lemonherb0323@naver.com>
2025-02-12 11:03:46 +0900

    :sparkles: Feat: 토큰 재발급 인터셉터 설정 (#54)

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -8,7 +8,7 @@ const instance = axios.create({
 });
 
 let isRefreshing = false; // 토큰 재발급 중인지 여부
-let refreshSubscribers: ((token: string) => void)[] = []; // 토큰 갱신 중인 요청 보관
+let refreshSubscribers: ((token: string) => void)[] = []; // 토큰 갱신 중인 요청 보관 (요청이 여러 개일 수 있으니 배열로 저장)
 
 // 401 오류 발생하면 accessToken 재발급
 instance.interceptors.response.use(
@@ -16,28 +16,33 @@ instance.interceptors.response.use(
   async (error) => {
     // 원래 요청 정보 저장
     const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (!originalRequest) {
+      return Promise.reject(error);
+    }
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
       if (isRefreshing) {
         // 토큰 재발급 시, 새 토큰 받을 때까지 대기
         return new Promise((resolve) => {
-          refreshSubscribers.push((token) => {
+          refreshSubscribers.push(() => {
             originalRequest._retry = true;
             resolve(instance(originalRequest)); // 재발급 되면 원래 요청 재시도
           });
         });
       }
-      originalRequest._retry = true;
+
       isRefreshing = true;
+      originalRequest._retry = true;
       try {
         // 토큰 재발급
         const response = await reissueAPI();
-        console.log(response);
+        console.log(response.data);
 
         isRefreshing = false;
         refreshSubscribers.forEach((callback) => callback(''));
         refreshSubscribers = [];
 
-        return instance(originalRequest);
+        return instance(originalRequest); // 원래 요청 다시 시도
       } catch (error) {
         isRefreshing = false;
         refreshSubscribers = [];
