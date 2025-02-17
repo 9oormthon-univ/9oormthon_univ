c9de8283813a6536793ea505abdd1dd07904ff12
gracekim527 <lemonherb0323@naver.com>
2025-02-17 19:43:20 +0900

    :ambulance: Hotfix: 서버 메시지가 뜨지 않는 현상 수정

--- src/pages/hackathon/IdeaApply/IdeaApply.tsx
+++ src/pages/hackathon/IdeaApply/IdeaApply.tsx
@@ -55,7 +55,7 @@ export default function IdeaApply() {
       }
     } catch (error: any) {
       if (error.response) {
-        const serverMessage = error.response.data.message;
+        const serverMessage = error.response.error.message;
         setErrorMessage(serverMessage || '알 수 없는 오류가 발생했습니다.');
       } else {
         console.error('Error applying idea:', error);
