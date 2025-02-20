efd8ad5b9713faf38190d33b1e5be8180ae28915
gracekim527 <lemonherb0323@naver.com>
2025-02-20 15:48:06 +0900

    :sparkles: Feat: 에디터 첨부파일 기능 추가 (#78)

--- src/components/hackathon/ideaForm/FormEditor.tsx
+++ src/components/hackathon/ideaForm/FormEditor.tsx
@@ -1,10 +1,11 @@
 import MDEditor, { bold, commands, hr, italic, strikethrough } from '@uiw/react-md-editor';
-import { Button, FormGroup } from '@goorm-dev/vapor-components';
+import { Button, FormGroup, Spinner, Text } from '@goorm-dev/vapor-components';
 import FormLabel from './FormLabel';
 import styles from './styles.module.scss';
 import { useState } from 'react';
-import { EditIcon, ViewOnIcon } from '@goorm-dev/vapor-icons';
+import { EditIcon, ImageOutlineIcon, ViewOnIcon } from '@goorm-dev/vapor-icons';
 import { useS3Upload } from '../../../hooks/useS3Upload';
+import useUpload from '../../../hooks/useUpload';
 
 interface FormEditorProps {
   label: string;
@@ -25,6 +26,7 @@ export default function FormEditor({
   const [markdownContent, setMarkdownContent] = useState(value);
   const { uploadToS3 } = useS3Upload();
   const [isUploading, setIsUploading] = useState(false);
+  const isImage = (file: File) => file.type.startsWith('image/');
 
   // 수정 버튼 클릭 시 편집 모드와 미리보기 모드 전환
   const toggleEditMode = () => {
@@ -44,12 +46,12 @@ export default function FormEditor({
     ),
   };
 
-  // 이미지 업로드
-  const handleImageUpload = async (image: File) => {
+  // 파일 업로드
+  const handleFileUpload = async (file: File) => {
     setIsUploading(true);
 
-    const imageName = image?.name || '이미지.png';
-    const loadingText = `<!-- Uploading "${imageName}"... -->`;
+    const fileName = file.name;
+    const loadingText = `<!-- Uploading "${fileName}"... -->`;
 
     setMarkdownContent((prev) => {
       const updatedContent = prev + `\n${loadingText}\n`;
@@ -57,14 +59,16 @@ export default function FormEditor({
       return updatedContent;
     });
 
-    const path = await uploadToS3(image);
+    const path = await uploadToS3(file);
     if (!path) {
       setIsUploading(false);
       return;
     }
 
+    const markdownText = isImage(file) ? `![](${encodeURI(path)})` : `[📄 ${fileName}](${encodeURI(path)})`;
+
     setMarkdownContent((prev) => {
-      const finalContent = prev.replace(loadingText, `![](${encodeURI(path)})`);
+      const finalContent = prev.replace(loadingText, markdownText);
       onChange(finalContent);
       return finalContent;
     });
@@ -78,61 +82,86 @@ export default function FormEditor({
     if (!files || !files.length) return;
 
     const image = files.item(0) as File;
-    await handleImageUpload(image);
+    await handleFileUpload(image);
+  };
+
+  // 첨부파일 박스를 통해 업로드 처리
+  const { upload } = useUpload();
+  const handleUpload = () => {
+    upload(async (file) => {
+      if (!file) return;
+      await handleFileUpload(file);
+    });
   };
 
   return (
     <FormGroup data-color-mode="light" onDragOver={(e) => e.preventDefault()}>
       <FormLabel label={label} nullable={nullable} />
-      <MDEditor
-        className={styles.editor}
-        value={markdownContent}
-        onChange={(val) => {
-          setMarkdownContent(val || '');
-          onChange(val || '');
-        }}
-        textareaProps={{
-          placeholder: placeholder,
-        }}
-        onPaste={async (e) => {
-          e.preventDefault();
-          await handlePasteOrDrop(e.clipboardData);
-        }}
-        onDrop={async (e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          await handlePasteOrDrop(e.dataTransfer);
-        }}
-        onDragOver={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-        }}
-        commands={[
-          bold,
-          italic,
-          strikethrough,
-          hr,
-          commands.group(
-            [commands.title1, commands.title2, commands.title3, commands.title4, commands.title5, commands.title6],
-            {
-              name: 'title',
-              groupName: 'title',
-              buttonProps: { 'aria-label': 'Insert title' },
-            },
-          ),
-          commands.divider,
-          commands.link,
-          commands.quote,
-          commands.code,
-          commands.codeBlock,
-        ]}
-        extraCommands={[editPreviewCommand]}
-        height={isEditing ? '100%' : '800px'}
-        minHeight={400}
-        preview={isEditing ? 'edit' : 'preview'}
-        visibleDragbar={false}
-      />
-      {isUploading && <div className={styles.uploading}>이미지 업로드 중...</div>}
+      <div className={styles.editorContainer}>
+        <MDEditor
+          className={styles.editor}
+          value={markdownContent}
+          onChange={(val) => {
+            setMarkdownContent(val || '');
+            onChange(val || '');
+          }}
+          textareaProps={{
+            placeholder: placeholder,
+          }}
+          onPaste={async (e) => {
+            e.preventDefault();
+            await handlePasteOrDrop(e.clipboardData);
+          }}
+          onDrop={async (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            await handlePasteOrDrop(e.dataTransfer);
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+          }}
+          commands={[
+            bold,
+            italic,
+            strikethrough,
+            hr,
+            commands.group(
+              [commands.title1, commands.title2, commands.title3, commands.title4, commands.title5, commands.title6],
+              {
+                name: 'title',
+                groupName: 'title',
+                buttonProps: { 'aria-label': 'Insert title' },
+              },
+            ),
+            commands.divider,
+            commands.link,
+            commands.quote,
+            commands.code,
+            commands.codeBlock,
+          ]}
+          extraCommands={[editPreviewCommand]}
+          height={isEditing ? '100%' : '600px'}
+          minHeight={400}
+          preview={isEditing ? 'edit' : 'preview'}
+          visibleDragbar={false}
+        />
+        {isEditing && (
+          <div className={styles.uploadBox} onClick={handleUpload}>
+            {isUploading ? (
+              <Spinner size="sm" />
+            ) : (
+              <>
+                <ImageOutlineIcon />
+              </>
+            )}
+
+            <Text typography="subtitle1" color="text-alternative">
+              이미지를 드래그하거나 이곳을 클릭해서 추가해주세요.
+            </Text>
+          </div>
+        )}
+      </div>
     </FormGroup>
   );
 }
