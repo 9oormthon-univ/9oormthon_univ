95500d0ea12d648c038553217cdbc4d6d0352c04
gracekim527 <lemonherb0323@naver.com>
2025-02-21 15:14:39 +0900

    :bug: Fix: 기간 세팅 api 호출 오류 수정 (#92)

--- src/api/system.ts
+++ src/api/system.ts
@@ -2,6 +2,6 @@ import instance from './instance';
 
 // 기간 조회 API
 export const fetchPeriod = async () => {
-  const response = await instance.get('/api/v1/system-setting/current-period');
+  const response = await instance.get('/api/v1/system-settings/current-period');
   return response.data;
 };
