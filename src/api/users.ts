3f15aad85331851e60b883e7c1903972dfb27acb
gracekim527 <lemonherb0323@naver.com>
2025-02-23 00:49:27 +0900

    :sparkles: Feat: 마이페이지 구현중 (#94)

---
+++ src/api/users.ts
@@ -0,0 +1,19 @@
+import instance from './instance';
+
+// 내 정보 조회
+export const getMyInfo = async () => {
+  const response = await instance.get('/api/v1/users/details');
+  return response.data;
+};
+
+// 유저 정보 조회
+export const getUserInfo = async (userId: number) => {
+  const response = await instance.get(`/api/v1/users/${userId}/details`);
+  return response.data;
+};
+
+// 유저정보 수정
+export const updateUserInfo = async (data: any) => {
+  const response = await instance.patch(`/api/v1/users`, data);
+  return response.data;
+};
