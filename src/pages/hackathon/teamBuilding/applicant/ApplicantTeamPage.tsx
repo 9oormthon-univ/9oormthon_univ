29165cfb583f3dabc4013aab7c22f76bbfdf4513
gracekim527 <lemonherb0323@naver.com>
2025-02-27 15:12:11 +0900

    :green_heart: Fix: role 로 인한 CI 에러 해결 (#104)

--- src/pages/hackathon/teamBuilding/applicant/ApplicantTeamPage.tsx
+++ src/pages/hackathon/teamBuilding/applicant/ApplicantTeamPage.tsx
@@ -1,9 +1,22 @@
+import { getTeamInfo } from '../../../../api/teams';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import BackLinkNavigation from '../../../../components/hackathon/common/BackLinkNavigation';
 import TeamInformation from '../../../../components/hackathon/teamBuilding/TeamInformation';
 import styles from './styles.module.scss';
 import { Text } from '@goorm-dev/vapor-components';
 
 export default function ApplicantTeamPage() {
+  const [teamInfo, setTeamInfo] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchTeamInfo = async () => {
+      const response = await getTeamInfo(4);
+      setTeamInfo(response.data);
+    };
+    fetchTeamInfo();
+  }, []);
+
   return (
     <div className={styles.teamPageContainer}>
       <BackLinkNavigation backLink="/team/applicant" />
@@ -11,7 +24,7 @@ export default function ApplicantTeamPage() {
         <Text as="h3" typography="heading3" color="text-normal">
           팀 정보
         </Text>
-        <TeamInformation viewer />
+        <TeamInformation viewer role={teamInfo?.role} />
       </div>
     </div>
   );
