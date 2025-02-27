113ee96fd28737803d7941e67a42bf1ab9022a5c
gracekim527 <lemonherb0323@naver.com>
2025-02-27 18:04:50 +0900

    :sparkles: Feat: 팀 이름 수정 기능 추가 (#111)

--- src/api/teams.ts
+++ src/api/teams.ts
@@ -5,3 +5,12 @@ export const getTeamInfo = async (generation: number) => {
   const response = await instance.get(`/api/v1/users/teams/details?generation=${generation}`);
   return response.data;
 };
+
+// 팀 정보 수정
+export const updateTeamInfo = async (generation: number, name: string) => {
+  const response = await instance.patch(`/api/v1/users/teams`, {
+    generation,
+    name,
+  });
+  return response.data;
+};
