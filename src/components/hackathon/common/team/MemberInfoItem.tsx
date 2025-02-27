342d23211a2ebf641c1cb1ab2936d9b08abf4dad
gracekim527 <lemonherb0323@naver.com>
2025-02-27 16:06:11 +0900

    :lipstick: Style: 팀원 정보 없음 스타일 수정 (#109)

--- src/components/hackathon/common/team/MemberInfoItem.tsx
+++ src/components/hackathon/common/team/MemberInfoItem.tsx
@@ -21,7 +21,11 @@ export default function MemberInfoItem({ name, imgUrl, id }: MemberInfoItemProps
           navigate(`/user/${id}`);
         }
       }}>
-      <div className={styles.memberInfoImg}>{imgUrl && <img src={imgUrl} alt="memberImg" />}</div>
+      {imgUrl && (
+        <div className={styles.memberInfoImg}>
+          <img src={imgUrl} alt="memberImg" />
+        </div>
+      )}
       <Text as="p" typography="body2" color="text-alternative">
         {name}
       </Text>
