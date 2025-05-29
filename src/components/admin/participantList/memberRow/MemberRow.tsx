4c46fcfd90e03c6751176c998e1f42b4a77bafc1
gracekim527 <lemonherb0323@naver.com>
2025-05-29 19:19:10 +0900

    :lipstick: Style: 안내 관련 모달 구현 (#139)

--- src/components/admin/participantList/memberRow/MemberRow.tsx
+++ src/components/admin/participantList/memberRow/MemberRow.tsx
@@ -2,64 +2,87 @@ import { DropdownItem, Dropdown, DropdownMenu, Text, DropdownToggle, Button } fr
 import styles from './styles.module.scss';
 import { MoreCommonOutlineIcon, ChevronRightOutlineIcon } from '@goorm-dev/vapor-icons';
 import { useState } from 'react';
+import InformationModal from '../../../common/modal/InformationModal';
 
 export const MemberRow = ({ member }: { member: any }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isRoleOpen, setIsRoleOpen] = useState(false);
-  const toggleDropdown = () => setIsOpen((prev) => !prev);
+  const [isInformationModalOpen, setIsInformationModalOpen] = useState(false);
+  const toggleDropdown = () => setIsDropdownOpen((prev) => !prev);
   const toggleRoleDropdown = () => setIsRoleOpen((prev) => !prev);
+  const toggleInformationModal = () => setIsInformationModalOpen((prev) => !prev);
 
   return (
-    <tr>
-      <td className={styles.memberName}>
-        <Text typography="subtitle1" color="text-normal">
-          {member.name}
-        </Text>
-        <Dropdown direction="down" className={styles.memberDropdown} isOpen={isOpen} toggle={toggleDropdown}>
-          <DropdownToggle size="sm" color="secondary" className={styles.memberDropdownToggle}>
-            <MoreCommonOutlineIcon className={styles.memberDropdownIcon} />
-          </DropdownToggle>
-          <DropdownMenu className={styles.memberDropdownMenu}>
-            <DropdownItem>
-              <Text typography="body2" as="p" color="text-normal">
-                정보 보기
-              </Text>
-            </DropdownItem>
-            <DropdownItem>
-              <Text typography="body2" as="p" color="text-danger">
-                퇴장
-              </Text>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </td>
-      <td style={{ width: '7.5rem' }}>
-        <Dropdown isOpen={isRoleOpen} toggle={toggleRoleDropdown} direction="down" size="sm">
-          <DropdownToggle color="secondary" outline caret>
-            {member.role}
-          </DropdownToggle>
-          <DropdownMenu right className={styles.memberRoleDropdownMenu}>
-            <DropdownItem>참가자</DropdownItem>
-            <DropdownItem>중앙운영단</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </td>
-      <td>
-        <Text typography="subtitle1" color="text-normal">
-          {member.email}
-        </Text>
-      </td>
-      <td>
-        <Text typography="subtitle1" color={member.teamBuilding ? 'text-success' : 'text-danger'}>
-          {member.teamBuilding ? '완료' : '미완료'}
-        </Text>
-      </td>
-      <td>
-        <Button size="sm" color="secondary">
-          정보 보기
-          <ChevronRightOutlineIcon className={styles.memberDropdownIcon} />
-        </Button>
-      </td>
-    </tr>
+    <>
+      <tr>
+        <td className={styles.memberName}>
+          <Text typography="subtitle1" color="text-normal">
+            {member.name}
+          </Text>
+          <Dropdown direction="down" className={styles.memberDropdown} isOpen={isDropdownOpen} toggle={toggleDropdown}>
+            <DropdownToggle size="sm" color="secondary" className={styles.memberDropdownToggle}>
+              <MoreCommonOutlineIcon className={styles.memberDropdownIcon} />
+            </DropdownToggle>
+            <DropdownMenu className={styles.memberDropdownMenu}>
+              <DropdownItem>
+                <Text typography="body2" as="p" color="text-normal">
+                  정보 보기
+                </Text>
+              </DropdownItem>
+              <DropdownItem onClick={toggleInformationModal}>
+                <Text typography="body2" as="p" color="text-danger">
+                  퇴장
+                </Text>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </td>
+        <td style={{ width: '7.5rem' }}>
+          <Dropdown isOpen={isRoleOpen} toggle={toggleRoleDropdown} direction="down" size="sm">
+            <DropdownToggle color="secondary" outline caret>
+              {member.role}
+            </DropdownToggle>
+            <DropdownMenu right className={styles.memberRoleDropdownMenu}>
+              <DropdownItem>참가자</DropdownItem>
+              <DropdownItem>중앙운영단</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </td>
+        <td>
+          <Text typography="subtitle1" color="text-normal">
+            {member.email}
+          </Text>
+        </td>
+        <td>
+          <Text typography="subtitle1" color={member.teamBuilding ? 'text-success' : 'text-danger'}>
+            {member.teamBuilding ? '완료' : '미완료'}
+          </Text>
+        </td>
+        <td>
+          <Button size="sm" color="secondary">
+            정보 보기
+            <ChevronRightOutlineIcon className={styles.memberDropdownIcon} />
+          </Button>
+        </td>
+      </tr>
+
+      <InformationModal
+        isOpen={isInformationModalOpen}
+        toggle={toggleInformationModal}
+        title="해당 미르미를 퇴장시키겠어요?"
+        description={
+          <>
+            <Text typography="body2" color="text-normal" as="p">
+              {member.name}을 미르미 리스트에서 삭제합니다.
+            </Text>
+            <Text typography="body2" color="text-normal" as="p">
+              미르미 퇴장이 완료되면 데이터를 되돌릴 수 없습니다.
+            </Text>
+          </>
+        }
+        confirmLabel="퇴장"
+        onConfirm={() => {}}
+      />
+    </>
   );
 };
