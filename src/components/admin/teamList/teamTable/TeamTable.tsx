3087b5ad3cb10954e91f4dac1c839aa71f153c22
gracekim527 <lemonherb0323@naver.com>
2025-05-30 21:02:15 +0900

    :lipstick: Style: teamTable 구현 (#142)

---
+++ src/components/admin/teamList/teamTable/TeamTable.tsx
@@ -0,0 +1,50 @@
+import { BasicPagination, Text } from '@goorm-dev/vapor-components';
+import styles from './teamTable.module.scss';
+import { TeamRow } from '../teamRow/TeamRow';
+
+export const TeamTable = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.tableContainer}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>
+                <Text typography="subtitle1" color="text-alternative">
+                  팀 번호
+                </Text>
+              </th>
+              <th>
+                <Text typography="subtitle1" color="text-alternative">
+                  팀 명
+                </Text>
+              </th>
+              <th>
+                <Text typography="subtitle1" color="text-alternative">
+                  아이디어 명
+                </Text>
+              </th>
+              <th>
+                <Text typography="subtitle1" color="text-alternative">
+                  인원 수
+                </Text>
+              </th>
+              <th>
+                <Text typography="subtitle1" color="text-alternative">
+                  팀 빌딩
+                </Text>
+              </th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <TeamRow />
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.pagination}>
+        <BasicPagination pageCount={10} />
+      </div>
+    </div>
+  );
+};
