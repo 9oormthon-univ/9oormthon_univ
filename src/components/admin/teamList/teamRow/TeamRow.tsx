fae1a536bbe02874a6256b0e82d11fb142e49660
gracekim527 <lemonherb0323@naver.com>
2025-05-31 15:48:06 +0900

    :lipstick: Style: 팀 리스트 리스트 자체 레이아웃 구현 (#142)

--- src/components/admin/teamList/teamRow/TeamRow.tsx
+++ src/components/admin/teamList/teamRow/TeamRow.tsx
@@ -1,13 +1,110 @@
-import { Text } from '@goorm-dev/vapor-components';
+import { DropdownToggle, DropdownMenu, Dropdown, DropdownItem, Text, Button } from '@goorm-dev/vapor-components';
+import styles from './teamRow.module.scss';
+import { ChevronRightOutlineIcon, MoreCommonOutlineIcon } from '@goorm-dev/vapor-icons';
+import { useState } from 'react';
 
 export const TeamRow = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const toggleDropdown = () => setIsDropdownOpen((prev) => !prev);
+
   return (
     <>
       <tr>
-        <td>
-          <Text typography="subtitle1" color="text-normal">
+        <td className={styles.teamName}>
+          <Text typography="body2" color="text-normal">
             1팀
           </Text>
+          <Dropdown direction="down" className={styles.memberDropdown} isOpen={isDropdownOpen} toggle={toggleDropdown}>
+            <DropdownToggle size="sm" color="secondary" className={styles.memberDropdownToggle}>
+              <MoreCommonOutlineIcon className={styles.memberDropdownIcon} />
+            </DropdownToggle>
+            <DropdownMenu className={styles.memberDropdownMenu}>
+              <DropdownItem>
+                <Text typography="body2" as="p" color="text-normal">
+                  정보 보기
+                </Text>
+              </DropdownItem>
+              <DropdownItem>
+                <Text typography="body2" as="p" color="text-danger">
+                  해체
+                </Text>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </td>
+        <td>
+          <Text typography="body2" color="text-normal">
+            온정
+          </Text>
+        </td>
+        <td>
+          <Text typography="body2" color="text-normal">
+            선행의 선순환이 시작되는 곳, 온정
+          </Text>
+        </td>
+        <td>
+          <Text typography="body2" color="text-normal">
+            6명
+          </Text>
+        </td>
+        <td>
+          <Text typography="body2" color="text-normal">
+            완료
+          </Text>
+        </td>
+        <td>
+          <Button size="sm" color="secondary" icon={ChevronRightOutlineIcon} iconSide="right">
+            팀원 관리
+          </Button>
+        </td>
+      </tr>
+      <tr>
+        <td className={styles.teamName}>
+          <Text typography="body2" color="text-normal">
+            1팀
+          </Text>
+          <Dropdown direction="down" className={styles.memberDropdown} isOpen={isDropdownOpen} toggle={toggleDropdown}>
+            <DropdownToggle size="sm" color="secondary" className={styles.memberDropdownToggle}>
+              <MoreCommonOutlineIcon className={styles.memberDropdownIcon} />
+            </DropdownToggle>
+            <DropdownMenu className={styles.memberDropdownMenu}>
+              <DropdownItem>
+                <Text typography="body2" as="p" color="text-normal">
+                  정보 보기
+                </Text>
+              </DropdownItem>
+              <DropdownItem>
+                <Text typography="body2" as="p" color="text-danger">
+                  해체
+                </Text>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </td>
+        <td>
+          <Text typography="body2" color="text-normal">
+            온정
+          </Text>
+        </td>
+        <td>
+          <Text typography="body2" color="text-normal">
+            선행의 선순환이 시작되는 곳, 온정
+          </Text>
+        </td>
+        <td>
+          <Text typography="body2" color="text-normal">
+            6명
+          </Text>
+        </td>
+        <td>
+          <Text typography="body2" color="text-normal">
+            완료
+          </Text>
+        </td>
+        <td>
+          <Button size="sm" color="secondary" icon={ChevronRightOutlineIcon} iconSide="right">
+            팀원 관리
+          </Button>
         </td>
       </tr>
     </>
