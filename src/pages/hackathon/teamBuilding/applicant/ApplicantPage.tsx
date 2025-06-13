2337b7fbfedbab9c1bae08a74e08f131a7925904
gracekim527 <lemonherb0323@naver.com>
2025-06-13 20:38:09 +0900

    :lipstick: Style: 지원자 뷰 로딩 스켈레톤 구현 (#176)

--- src/pages/hackathon/teamBuilding/applicant/ApplicantPage.tsx
+++ src/pages/hackathon/teamBuilding/applicant/ApplicantPage.tsx
@@ -1,32 +1,39 @@
 import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
-import { Text } from '@goorm-dev/vapor-components';
+import { Skeleton, Text } from '@goorm-dev/vapor-components';
 import IdeaApplyListItem from '../../../../components/hackathon/teamBuilding/IdeaApplyListItem';
 import { getMyApplySummary } from '../../../../api/users';
 import usePeriodStore from '../../../../store/usePeriodStore';
 import TeamBuildingPhaseSelector from '../../../../components/hackathon/teamBuilding/TeamBuildingPhaseSelector';
 import { GENERATION } from '../../../../constants/common';
+import IdeaApplyListSkeleton from '../../../../components/hackathon/teamBuilding/skeletonLoading/IdeaApplyListSkeleton';
 
 export default function ApplicantPage() {
   const [buttonIndex, setButtonIndex] = useState(0);
   const [applySummary, setApplySummary] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 현재 팀빌딩 기간 조회
   const { current_phase, fetchPeriodData } = usePeriodStore();
 
-  useEffect(() => {
-    fetchPeriodData();
-  }, []);
-
   // 지원 내역 조회
   const fetchApplySummary = async () => {
     try {
+      setIsLoading(true);
       const response = await getMyApplySummary(GENERATION, current_phase);
       setApplySummary(response.data);
     } catch (error) {
       console.error('Error fetching apply summary:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchPeriodData();
+    setIsLoading(false);
+  }, []);
 
   useEffect(() => {
     if (current_phase) {
@@ -39,9 +46,14 @@ export default function ApplicantPage() {
       <Text as="h3" typography="heading3">
         팀 빌딩 현황
       </Text>
-      <TeamBuildingPhaseSelector onPhaseChange={setButtonIndex} activeIndex={buttonIndex} />
-
-      {applySummary?.applies?.length > 0 ? (
+      {isLoading ? (
+        <Skeleton width="23.4375rem" height="3rem" />
+      ) : (
+        <TeamBuildingPhaseSelector onPhaseChange={setButtonIndex} activeIndex={buttonIndex} />
+      )}
+      {isLoading ? (
+        <IdeaApplyListSkeleton />
+      ) : applySummary?.applies?.length > 0 ? (
         applySummary?.applies?.map((apply: any) => (
           <IdeaApplyListItem
             key={apply.apply_info.id}
