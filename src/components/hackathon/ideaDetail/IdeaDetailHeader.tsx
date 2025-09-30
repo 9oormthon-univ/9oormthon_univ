fb4e5b9a56cca21d6a9ad628b258dcb196302a3f
gracekim527 <k07173027@gmail.com>
2025-09-30 16:48:09 +0900

    :recycle: Refactor: 아이디어 삭제 query화 (#256)

--- src/components/hackathon/ideaDetail/IdeaDetailHeader.tsx
+++ src/components/hackathon/ideaDetail/IdeaDetailHeader.tsx
@@ -12,12 +12,12 @@ import styles from './styles.module.scss';
 import { BookmarkIcon, BookmarkOutlineIcon, MoreCommonOutlineIcon, OutOutlineIcon } from '@goorm-dev/vapor-icons';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import useBreakPoint from '../../../hooks/useBreakPoint';
-import usePeriodStore from '../../../store/usePeriodStore';
-import useAuthStore from '../../../store/useAuthStore';
-import { UserStatus } from '../../../constants/role';
-import { deleteIdea } from '../../../api/idea';
+import useBreakPoint from '@/hooks/useBreakPoint';
+import usePeriodStore from '@/store/usePeriodStore';
+import useAuthStore from '@/store/useAuthStore';
+import { UserStatus } from '@/constants/role';
 import InformationModal from '../../common/modal/InformationModal';
+import { useDeleteIdeaMutation } from '@/hooks/mutations/useIdeaMutations';
 interface IdeaDetailHeaderProps {
   id: number;
   provider_id: number;
@@ -50,45 +50,44 @@ export default function IdeaDetailHeader({
   const navigate = useNavigate();
   const breakpoint = useBreakPoint();
   const { current_period, isTeamBuildingPeriod } = usePeriodStore();
-  const { status, fetchUserStatus } = useAuthStore();
+  const { status } = useAuthStore();
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const { mutate: deleteIdea } = useDeleteIdeaMutation();
 
   // 아이디어 지원 이동
   const handleApplyIdea = () => {
-    if (!isTeamBuildingPeriod() || current_period === 'HACKATHON') {
-      toast('팀 빌딩 지원 기간이 아닙니다.', {
+    if (!isTeamBuildingPeriod || current_period === 'HACKATHON') {
+      return toast('팀 빌딩 지원 기간이 아닙니다.', {
         type: 'danger',
       });
-      return;
     }
-    // 제시자나 멤버가 아니면 지원 가능
+
     if (status !== UserStatus.PROVIDER && status !== UserStatus.MEMBER) {
-      navigate(`/hackathon/apply/${id}`);
-    } else if (status === UserStatus.PROVIDER) {
-      toast('이미 등록한 아이디어가 있어 지원할 수 없습니다.', {
-        type: 'danger',
-      });
-    } else if (status === UserStatus.MEMBER) {
-      toast('이미 다른 팀에 소속되어 있어 지원할 수 없습니다.', {
-        type: 'danger',
-      });
-    } else {
-      toast('지원 가능한 상태가 아닙니다.', {
+      return navigate(`/hackathon/apply/${id}`);
+    }
+
+    if (status === UserStatus.PROVIDER) {
+      return toast('이미 등록한 아이디어가 있어 지원할 수 없습니다.', {
         type: 'danger',
       });
     }
+
+    if (status === UserStatus.MEMBER) {
+      return toast('이미 다른 팀에 소속되어 있어 지원할 수 없습니다.', {
+        type: 'danger',
+      });
+    }
+
+    toast('지원 가능한 상태가 아닙니다.', {
+      type: 'danger',
+    });
   };
 
   // 아이디어 삭제
   const handleDeleteIdea = async () => {
     if (current_period === 'IDEA_SUBMISSION') {
-      await deleteIdea(id);
-      await fetchUserStatus(); // 사용자 상태 갱신
+      deleteIdea({ id });
       setIsDeleteModalOpen(false);
-      navigate('/hackathon');
-      toast('아이디어를 삭제했습니다.', {
-        type: 'primary',
-      });
     } else {
       setIsDeleteModalOpen(false);
       toast('아이디어 삭제 기간이 아닙니다.', {
--- src/hooks/mutations/useIdeaMutations.ts
+++ src/hooks/mutations/useIdeaMutations.ts
@@ -1,11 +1,15 @@
 // src/hooks/mutations/useIdeaMutations.ts
 
-import { useMutation } from '@tanstack/react-query';
-import { createIdeaAPI, updateIdeaAPI } from '@/api/idea';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createIdeaAPI, deleteIdea, updateIdeaAPI } from '@/api/idea';
 import { toast } from '@goorm-dev/vapor-components';
 import { IdeaCreateEdit } from '@/types/user/idea';
 import { IDEA_ADD_ERROR_MESSAGES } from '@/constants/errorMessage';
+import { useNavigate } from 'react-router-dom';
+import useAuthStore from '@/store/useAuthStore';
+import { UserBrief } from '@/types/user/users';
 
+// 아이디어 등록
 export const useCreateIdeaMutation = () => {
   return useMutation<void, unknown, IdeaCreateEdit>({
     mutationFn: (data: IdeaCreateEdit) => createIdeaAPI(data),
@@ -19,12 +23,49 @@ export const useCreateIdeaMutation = () => {
   });
 };
 
+// 아이디어 수정
 export const useUpdateIdeaMutation = () => {
   return useMutation<void, unknown, { data: IdeaCreateEdit; id: number }>({
     mutationFn: ({ data, id }: { data: IdeaCreateEdit; id: number }) => updateIdeaAPI(data, id),
     onError: (error: any) => {
       const serverCode = error?.response?.data?.error?.code as keyof typeof IDEA_ADD_ERROR_MESSAGES | undefined;
       const message = (serverCode && IDEA_ADD_ERROR_MESSAGES[serverCode]) || '알 수 없는 오류가 발생하였습니다.';
+      toast(message, {
+        type: 'danger',
+      });
+    },
+  });
+};
+
+// 아이디어 삭제
+export const useDeleteIdeaMutation = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const updateUser = useAuthStore((state) => state.updateUserFromQuery);
+
+  return useMutation<void, unknown, { id: number }>({
+    mutationFn: ({ id }: { id: number }) => deleteIdea(id),
+    onSuccess: async (_data, { id }) => {
+      await queryClient.invalidateQueries({ queryKey: ['user'] });
+
+      const user = await queryClient.getQueryData(['user']);
+      if (user) {
+        updateUser(user as UserBrief);
+      }
+
+      await Promise.all([
+        queryClient.invalidateQueries({ queryKey: ['ideas'] }),
+        queryClient.invalidateQueries({ queryKey: ['idea', id] }),
+      ]);
+
+      navigate('/hackathon');
+      toast('아이디어를 삭제했습니다.', {
+        type: 'primary',
+      });
+    },
+    onError: (error: any) => {
+      const serverMessage = error?.response?.data?.error?.message;
+      const message = serverMessage || '알 수 없는 오류가 발생하였습니다.';
       toast(message, {
         type: 'danger',
       });
